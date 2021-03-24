826c57b7422e2cc94a1ad380ca901af3a10fc345
tutajm <mtutaj@mcw.edu>
2021-03-23 19:39:16 -0500

    gp2protein.rgd: discontinued submission to GO svn endpoint due to its unavailability

--- src/main/dist/gp2proteinExtract.sh
+++ src/main/dist/gp2proteinExtract.sh
@@ -1,5 +1,3 @@
-# analyzes rat, mouse and human sslps and generates public ftp files SSLPS_RAT, SSLPS_MOUSE and SSLPS_HUMAN
-# abort the script if any of stages below will fail
 set -e
 
 APPHOME=/home/rgddata/pipelines/ftpFileExtracts
